4feb752dc9610f955d571fc61fd5f72df82390ea
Troy Harrison <KuroNoPardalis@gmail.com>
2018-04-26 14:56:33 +0200

    Add endpoints for user

--- backend/api/endpoints/users/user.py
+++ backend/api/endpoints/users/user.py
@@ -9,3 +9,27 @@ USER = NAMESPACE.model('User', {
     'server_ids': fields.String(readOnly=True, description="The server that the user is tied to"),
 
 })
+
+@NAMESPACE.route('/<string:id>')
+@NAMESPACE.response(404, 'User not found')
+@NAMESPACE.param('id', 'The user identifier')
+class User(Resource):
+    '''Show a single user and lets you delete a single user'''
+    @NAMESPACE.doc('get_user')
+    @NAMESPACE.marshal_with(USER)
+    def get(self, id):
+        '''Fetch a given resource'''
+        return "get"
+
+    @NAMESPACE.doc('delete_user')
+    @NAMESPACE.response(204, 'User deleted')
+    def delete(self, id):
+        '''Delete a user given its identifier'''
+        return "delete"
+        return '', 204
+
+    @NAMESPACE.expect(USER)
+    @NAMESPACE.marshal_with(USER)
+    def put(self, id):
+        '''Update a user given its identifier'''
+        return "request"
