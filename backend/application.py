0914242c5b250a2d387dfc6607a450a7e74cd3e8
Troy Harrison <KuroNoPardalis@gmail.com>
2018-04-29 15:05:34 +0200

    Add cli interaction to run tests

--- backend/application.py
+++ backend/application.py
@@ -2,6 +2,7 @@
 Coordinates the running of the Hestia web backend.
 """
 import click
+import pytest
 from flask import Flask
 from flask_cors import CORS
 from werkzeug.contrib.fixers import ProxyFix
@@ -36,6 +37,12 @@ def dev():
     """
     APP.run(host="0.0.0.0", port=5000, debug=True)
 
+@click.command()
+def test():
+    """
+    Run the tests.
+    """
+    pytest.main(["-s", "tests/"])
 
 @click.command()
 @click.option('--dbtype',
@@ -52,6 +59,7 @@ def initdb(dbtype):
 cli.add_command(run)
 cli.add_command(dev)
 cli.add_command(initdb)
+cli.add_command(test)
 
 if __name__ == "__main__":
     cli()
