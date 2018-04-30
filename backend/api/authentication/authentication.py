f390b9f8a528c619e4af755b119678d35c2e7c05
Troy Harrison <KuroNoPardalis@gmail.com>
2018-04-30 02:53:07 +0200

    Add ability to access endpoints only when passing correct authorization token

--- backend/api/authentication/authentication.py
+++ backend/api/authentication/authentication.py
@@ -81,6 +81,7 @@ def requires_auth(f):
     """
     @wraps(f)
     def decorated(*args, **kwargs):
+
         token = get_token_auth_header()
         jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
         jwks = json.loads(jsonurl.read())
