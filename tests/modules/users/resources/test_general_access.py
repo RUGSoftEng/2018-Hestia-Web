26c131e466e3ce9dac14eac7615fa9b38d7c1d66
Troy Harrison <KuroNoPardalis@gmail.com>
2018-05-19 16:01:42 +0200

    Add docstring

--- tests/modules/users/resources/test_general_access.py
+++ tests/modules/users/resources/test_general_access.py
@@ -8,5 +8,6 @@ import pytest
     ('POST', '/users/'),
 ))
 def test_general_access(http_method, http_path, flask_app_client):
+    """ Test that it is possible to access all of the user endpoints """
     response = flask_app_client.open(method=http_method, path=http_path)
     assert response.status_code == 200
