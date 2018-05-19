95f3aee96b80e0617e65712ce17c32b6c9e233d1
Phil Oetinger <p.oetinger@student.rug.nl>
2018-05-19 15:48:21 +0200

    Remove a single line

--- tests/modules/users/resources/test_general_access.py
+++ tests/modules/users/resources/test_general_access.py
@@ -6,7 +6,6 @@ import pytest
 
 @pytest.mark.parametrize('http_method,http_path', (
     ('POST', '/users/'),
-
 ))
 def test_general_access(http_method, http_path, flask_app_client):
     response = flask_app_client.open(method=http_method, path=http_path)
