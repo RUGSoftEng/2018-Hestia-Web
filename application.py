6e32f3db25c639e3fbdd20b129c648ce3c0afeaf
Troy Harrison <KuroNoPardalis@gmail.com>
2018-05-25 10:33:00 +0200

    Heroku fixes

--- application.py
+++ application.py
@@ -2,7 +2,9 @@
 Coordinates the CLI for the application.
 """
 import click
+import os
 from app import (create_app)
+PORT = int(os.environ.get("PORT", 5000))
 
 @click.group()
 def cli():
@@ -13,13 +15,13 @@ def cli():
 def run():
     """ Run the backend in production mode. """
     app = create_app('production')
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=PORT)
 
 @click.command()
 def dev():
     """ Run the backend in development mode. """
     app = create_app('development')
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=PORT)
 
 
 cli.add_command(run)
