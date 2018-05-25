bcb205b8661776ce67541aba3a150622d8825eec
Troy Harrison <KuroNoPardalis@gmail.com>
2018-05-25 10:26:48 +0200

    Reconfigure application temporarily to sqlite for production

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ class TestingConfig(BaseConfig):
 class ProductionConfig(BaseConfig):
     """ Production configuration for the application. """
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_BASE_URL}/HestiaDB'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///HestiaDB'
     PORT = int(os.environ.get("PORT", 5000))
 
 
