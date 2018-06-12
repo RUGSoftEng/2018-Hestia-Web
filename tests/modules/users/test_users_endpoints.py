45aba75d01d5249fca8cdb6c925d39cf37c74829
Rens <rtnijman@gmail.comrtnijman@gmail.com>
2018-06-12 13:47:10 +0200

    replace unnecessary parameterization

--- tests/modules/users/test_users_endpoints.py
+++ tests/modules/users/test_users_endpoints.py
@@ -3,19 +3,18 @@ Test user endpoints.
 """
 import pytest
 
-
-@pytest.mark.parametrize('http_method,http_path', (
-    ('POST', '/users/'),
-    ('DELETE', '/users/1'),
-))
-def test_POST_users(http_method, http_path, flask_app_client):
+def test_POST_users(flask_app_client):
     """ Test that it is possible to access all of the user endpoints """
+    http_method = 'POST'
+    http_path = '/users/1'
     response = flask_app_client.open(method=http_method, path=http_path)
     print(response.get_json())
     assert response.get_json() is not None
 
-def test_DELETE_users(http_method, http_path, flask_app_client):
+def test_DELETE_users(flask_app_client):
     """ Test that it is possible to access all of the user endpoints """
+    http_method = 'DELETE'
+    http_path = '/users/1'
     response = flask_app_client.open(method=http_method, path=http_path)
     print(response.get_json())
     assert response.get_json() is not None
